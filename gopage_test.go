1eb2165417480b72452e2ea69548bb61ef69af2b
harshad <harshad0402@gmail.com>
2016-03-06 01:41:21 +0530

    increased test coverage

--- gopage_test.go
+++ gopage_test.go
@@ -6,7 +6,31 @@ import (
 )
 
 func TestNewPaginator(t *testing.T) {
-	p, err := NewPaginator([]int{1, 2, 3, 4})
+	source := []int{1, 2, 3, 4}
+	p, err := NewPaginator(source)
 	assert.Nil(t, err)
 	assert.Equal(t, p.GetPageSize(), DefaultPageSize)
+
+	sl, errsl := p.Page(1)
+	assert.Nil(t, errsl)
+	assert.NotEqual(t, len(sl), 0)
+	assert.Equal(t, len(sl), 4)
+	for i, s := range sl {
+		assert.Equal(t, s.(int), source[i])
+	}
+
+	p.SetPageSize(2)
+	assert.Equal(t, p.GetPageSize(), 2)
+	sl, errsl = p.Page(1)
+	assert.Nil(t, errsl)
+	assert.NotEqual(t, len(sl), 0)
+	assert.Equal(t, len(sl), 2)
+	for i, s := range sl {
+		assert.Equal(t, s.(int), source[i])
+	}
+
+	sl, errsl = p.Page(3)
+	assert.Equal(t, errsl, ErrOverflow)
+	assert.Equal(t, len(sl), 0)
+	assert.Nil(t, sl)
 }
