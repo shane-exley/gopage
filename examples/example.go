4663d126252d34683810b7cfad7ccc1757300481
harshad <harshad0402@gmail.com>
2016-03-06 02:45:06 +0530

    added example file

---
+++ examples/example.go
@@ -0,0 +1,15 @@
+package gopage_test
+
+import (
+	"fmt"
+	"github.com/goibibo/gopage"
+)
+
+func main() {
+	p, _ := gopage.NewPaginator([]int{1, 2, 3, 4, 5})
+	p.SetPageSize(2)
+
+	fmt.Println(p.Page(1))
+	fmt.Println(p.Page(2))
+	fmt.Println(p.Page(3))
+}
