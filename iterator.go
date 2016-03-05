510ac55e76a77447a2daefd83b6d3a33377ee69a
harshad <harshad0402@gmail.com>
2016-03-06 02:17:40 +0530

    added comments to iterator struct and its methods

--- iterator.go
+++ iterator.go
@@ -4,20 +4,24 @@ import (
 	"sync"
 )
 
+//Iterator Simple object to iteratie over a pager
 type Iterator struct {
 	paginator *Paginator
 	pageCount int
 	mux       sync.Mutex
 }
 
+//GetIterator Generates an Iterator over a Pager
 func (p *Paginator) GetIterator() (*Iterator, error) {
 	return &Iterator{paginator: p}, nil
 }
 
+//GetCursorPosition Gets the current page number
 func (i *Iterator) GetCursorPosition() int {
 	return i.pageCount
 }
 
+//Next Gets the Next Page
 func (i *Iterator) Next() ([]interface{}, error) {
 	i.mux.Lock()
 	defer i.mux.Unlock()
@@ -26,6 +30,7 @@ func (i *Iterator) Next() ([]interface{}, error) {
 	return i.paginator.Page(i.pageCount)
 }
 
+//Prev Gets the previous page. Also takes the cursor back
 func (i *Iterator) Prev() ([]interface{}, error) {
 	i.mux.Lock()
 	defer i.mux.Unlock()
