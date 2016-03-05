a59d3df2966e0c759a45cd13dc58da5e11d21af6
harshad <harshad0402@gmail.com>
2016-03-06 01:47:27 +0530

    increased test coverage for all exported errors

--- gopage_test.go
+++ gopage_test.go
@@ -19,7 +19,8 @@ func TestNewPaginator(t *testing.T) {
 		assert.Equal(t, s.(int), source[i])
 	}
 
-	p.SetPageSize(2)
+	errps := p.SetPageSize(2)
+	assert.Nil(t, errps)
 	assert.Equal(t, p.GetPageSize(), 2)
 	sl, errsl = p.Page(1)
 	assert.Nil(t, errsl)
@@ -29,8 +30,19 @@ func TestNewPaginator(t *testing.T) {
 		assert.Equal(t, s.(int), source[i])
 	}
 
+	errps = p.SetPageSize(-10)
+	assert.NotNil(t, errps)
+	assert.Equal(t, errps, ErrInvalidPageSize)
+	ps := p.GetPageSize()
+	assert.Equal(t, ps, 2)
+
 	sl, errsl = p.Page(3)
 	assert.Equal(t, errsl, ErrOverflow)
 	assert.Equal(t, len(sl), 0)
 	assert.Nil(t, sl)
+
+	d, errd := NewPaginator(1234)
+	assert.NotNil(t, errd)
+	assert.Equal(t, errd, ErrNotSlice)
+	assert.Nil(t, d)
 }
